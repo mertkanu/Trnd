e01f033fb1e4ffcf52ac43438bcaeab0ffe88ede
mertkanuMynet <mertkan.uzunparmak@mynetgroup.com>
2019-05-09 15:59:02 +0300

    Implement core libraries

---
+++ data/src/main/java/com/cincoy/data/api/RemoteServiceApi.kt
@@ -0,0 +1,8 @@
+package com.cincoy.data.api
+
+/**
+ * Created by mertkanuzunparmak on 2019-05-09.
+ * mertkanu@gmail.com
+ */
+interface RemoteServiceApi {
+}
---
+++ data/src/main/java/com/cincoy/data/db/LocalDatabase.kt
@@ -0,0 +1,10 @@
+package com.cincoy.data.db
+
+import androidx.room.RoomDatabase
+
+/**
+ * Created by mertkanuzunparmak on 2019-05-09.
+ * mertkanu@gmail.com
+ */
+abstract class LocalDatabase : RoomDatabase() {
+}
